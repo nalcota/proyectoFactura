ba1fbbd9dd4e971b41458834fa2a9f1a4f7d870e
nalcota <ni.alcota@duoucuc.cl>
2023-10-24 20:51:26 -0300

    Maquetando los datos del componente InvoiceApp

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -2,14 +2,33 @@ import { getInvoice } from "../services/getInvoice";
 
 export const InvoiceApp = () => {
 
-    const invoice = getInvoice();
+    const { id, name, client, company, items} = getInvoice();
+
+    const {name: nameClient, lastName, address} = client;
+    const { country, city, street, number} = address;
     return (
         <>
         <h1>Ejemplo Factura</h1>
         <ul>
-            <li>Id: { invoice.id }</li>
-            <li>Name: { invoice.name }</li>
+            <li>Id: { id }</li>
+            <li>Name: { name }</li>
         </ul>
+
+        <h3> Datos del cliente</h3>
+        <ul>
+            <li>{ nameClient} { lastName }</li>
+            <li>{ country}</li>
+            <li>{ city}</li>
+            <li>{ street} { number}</li>
+        </ul>
+
+        <h3>Datos de la empresa</h3>
+        <ul>
+            <li>{company.name}</li>
+            <li>{company.fiscalNumber}</li>
+        </ul>
+
+        <h4> Productos de la factura </h4>
         </>
     )
 }
