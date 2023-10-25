d860a5e21ae2cd92436fd3680d61953f5408fd8c
nalcota <ni.alcota@duoucuc.cl>
2023-10-24 21:34:34 -0300

    Agregando estilo CSS con Bootstrap

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -2,54 +2,66 @@ import { getInvoice } from "../services/getInvoice";
 
 export const InvoiceApp = () => {
 
-    const { id, name, client, company, items} = getInvoice();
+    const { id, name, client, company, items } = getInvoice();
 
-    const {name: nameClient, lastName, address} = client;
-    const { country, city, street, number} = address;
+    const { name: nameClient, lastName, address } = client;
+    const { country, city, street, number } = address;
     return (
         <>
-        <h1>Ejemplo Factura</h1>
-        <ul>
-            <li>Id: { id }</li>
-            <li>Name: { name }</li>
-        </ul>
+            <div className="container">
 
-        <h3> Datos del cliente</h3>
-        <ul>
-            <li>{ nameClient} { lastName }</li>
-            <li>{ country}</li>
-            <li>{ city}</li>
-            <li>{ street} { number}</li>
-        </ul>
+                <div className="card my-3">
+                    <div className="card-header">
+                        Ejemplo Factura
+                    </div>
+                    <div className="card-body">
+                        <ul className="list-group">
+                            <li className="list-group-item">Id: {id}</li>
+                            <li className="list-group-item">Name: {name}</li>
+                        </ul>
+                        <div className="row my-3">
+                            <div className="col">
+                                <h3> Datos del cliente</h3>
+                                <ul className="list-group">
+                                    <li className="list-group-item active">{nameClient} {lastName}</li>
+                                    <li className="list-group-item">{country} / {city}</li>
+                                    <li className="list-group-item">{street} {number}</li>
+                                </ul>
+                            </div>
+                            <div className="col">
+                                <h3>Datos de la empresa</h3>
+                                <ul className="list-group">
+                                    <li className="list-group-item active">{company.name}</li>
+                                    <li className="list-group-item">{company.fiscalNumber}</li>
+                                </ul>
+                            </div>
+                        </div>
 
-        <h3>Datos de la empresa</h3>
-        <ul>
-            <li>{company.name}</li>
-            <li>{company.fiscalNumber}</li>
-        </ul>
+                        <h4> Productos de la factura </h4>
+                        <table className="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                </tr>
 
-        <h4> Productos de la factura </h4>
-        <table>
-            <thead>
-                <tr>
-                <th>Producto</th>
-                <th>Precio</th>
-                <th>Cantidad</th>
-                </tr>
-                
-            </thead>
-            <tbody>
-                { items.map(({id, product, price, quantity}) => (
-                <tr key={id}>
-                    <td>{product}</td>
-                    <td>{price}</td>
-                    <td>{quantity}</td>
-                </tr>
+                            </thead>
+                            <tbody>
+                                {items.map(({ id, product, price, quantity }) => (
+                                    <tr key={id}>
+                                        <td>{product}</td>
+                                        <td>{price}</td>
+                                        <td>{quantity}</td>
+                                    </tr>
 
-                    ))}
-                
-            </tbody>
-        </table>
+                                ))}
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
